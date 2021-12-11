9e47de88135c0346c04a62771870a1c227038a2a
ipurpl3x <rafael.horvat@icloud.com>
2021-12-11 18:33:36 +0100

    Removed styles import (was broken)

--- app/src/pages/Circles/index.tsx
+++ app/src/pages/Circles/index.tsx
@@ -1,12 +1,6 @@
 import { FC } from 'react'
 import CirclesPage from './CirclesPage'
 
-// Styling
-import './less/tribes.less'
-import './less/tribes-grid.less'
-import './less/tribe.less'
-import './less/tribe-badge.less'
-
 const Circles: FC = () => {
     return (
       <div>
