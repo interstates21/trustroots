faddaaf5a3d8103f3d88d03dbd6b294049a30539
Alex Kupin <interstates21@gmail.com>
2021-12-15 11:58:40 +0100

    Map Filters

--- app/src/pages/Search/MapBar/index.tsx
+++ app/src/pages/Search/MapBar/index.tsx
@@ -1,18 +1,36 @@
-import { Tag } from '@chakra-ui/react';
-import { FC } from 'react';
+import { Button, Tag, TagCloseButton, TagLabel } from '@chakra-ui/react';
+import { FC, useState } from 'react';
 import styles from './MapBar.module.scss';
 import circlesData from '../../../api/circles/data.json';
 
 interface Props {}
 
+const FILTERS = ['Recently Active', 'Cyclists', 'Hackers'];
+
+const Filter: FC<{ label: string }> = ({ label }) => {
+  const [active, setActive] = useState(false);
+  return (
+    <Button
+      borderRadius="full"
+      colorScheme="blackAlpha"
+      className={styles.filterButton}
+      size="sm"
+      onClick={() => setActive((p) => !p)}
+      isActive={active}
+    >
+      {label}
+    </Button>
+  );
+};
+
 const MapBar: FC<Props> = () => {
+  // const [filters, setFilters] = useState([]);
+
   console.log('circlesData =', circlesData);
   return (
     <div className={styles.mapBar}>
-      {circlesData.map((circle) => (
-        <Tag style={{ width: 'min-content', minWidth: 100, margin: 5 }} key={circle.id}>
-          {circle.label}
-        </Tag>
+      {FILTERS.map((filter) => (
+        <Filter label={filter} key={filter} />
       ))}
     </div>
   );
